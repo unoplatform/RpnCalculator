0fc372e17435c2c55132ae1468143945f18f17c9
Jerome Laban <jerome.laban@nventive.com>
2019-09-06 09:53:55 -0400

    Initial sources.
    Based on 2db8607f75d8884d1dc424cb4d8ae7a81dff76ec

---
+++ RpnCalculator/RpnCalculator.Wasm/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using Windows.UI.Xaml;
+
+namespace RpnCalculator.Wasm
+{
+    public class Program
+    {
+        static int Main(string[] args)
+        {
+            ConfigureFilters(Uno.Extensions.LogExtensionPoint.AmbientLoggerFactory);
+
+            Windows.UI.Xaml.Application.Start(_ => new RpnCalculator.UWP.App());
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Configures global logging
+        /// </summary>
+        /// <param name="factory"></param>
+        static void ConfigureFilters(ILoggerFactory factory)
+        {
+            factory
+                .WithFilter(new FilterLoggerSettings
+                    {
+                        { "Uno", LogLevel.Warning },
+                        { "Windows", LogLevel.Warning },
+
+						// Debug JS interop
+						// { "Uno.Foundation.WebAssemblyRuntime", LogLevel.Debug },
+
+						// Generic Xaml events
+						// { "Windows.UI.Xaml", LogLevel.Debug },
+						// { "Windows.UI.Xaml.VisualStateGroup", LogLevel.Debug },
+						// { "Windows.UI.Xaml.StateTriggerBase", LogLevel.Debug },
+						// { "Windows.UI.Xaml.UIElement", LogLevel.Debug },
+
+						// Layouter specific messages
+						// { "Windows.UI.Xaml.Controls", LogLevel.Debug },
+						// { "Windows.UI.Xaml.Controls.Layouter", LogLevel.Debug },
+						// { "Windows.UI.Xaml.Controls.Panel", LogLevel.Debug },
+						// { "Windows.Storage", LogLevel.Debug },
+
+						// Binding related messages
+						// { "Windows.UI.Xaml.Data", LogLevel.Debug },
+
+						// DependencyObject memory references tracking
+						// { "ReferenceHolder", LogLevel.Debug },
+					}
+                )
+#if DEBUG
+                .AddConsole(LogLevel.Debug);
+#else
+                .AddConsole(LogLevel.Information);
+#endif
+        }
+    }
+}
